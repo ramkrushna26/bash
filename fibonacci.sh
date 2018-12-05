9a5927cae30cd3b050d5b38ba799395ebc2e2cde
ramkrushna26 <45620457+ramkrushna26@users.noreply.github.com>
2018-12-05 13:26:36 +0530

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+####################################################################################
+#             Name:         fibonacci.sh                                           #
+#             Description:  Generating pattern                                     #
+#             Usage:        ./scriptname                                           #
+#             Author:       Ramkrushna Bolewar                                     #
+#             Version:      1.0                                                    #
+####################################################################################
+#                              Version History                                     #
+####################################################################################
+# Version |   Date   |                Change                       | Author        #
+####################################################################################
+#   1.0   | 15/05/17 |            Initial Version                  | Ramkrushna    #
+####################################################################################
+
+
+########variables
+
+read -p "Enter the number : " input
+export input
+export count=0
+first=0
+second=1
+
+########functions
+
+fibonacci() {
+        echo -ne " $first "
+        count=$(( $count + 1 ))
+        echo -ne " $second "
+        count=$(( $count + 1 ))
+
+        while [ $input -ne $count ]
+        do
+                temp=$(( $first + $second ))
+                first=$(( $second ))
+                second=$(( $temp ))
+                echo -ne " $second "
+                count=$(( $count + 1 ))
+        done
+
+        echo " BATMAN "
+}
+
+if [ $input -eq 1 ];then
+        echo $first
+elif [ $input -gt 1 ];then
+        fibonacci $input
+else
+        echo "Entered wrong number, I guess you dont want to play. BYE"
+        break;
+fi
