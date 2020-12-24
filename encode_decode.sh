2d1201b6e37771e828867b52b789e508faa05714
Ramkrushna Bolewar <ramkrushna.bolewar1@gmail.com>
2020-12-24 19:06:30 +0800

    Create encode_decode.sh

---
+++ encode_decode.sh
@@ -0,0 +1,5 @@
+my_var="String to encode"
+
+encrypted_pwd=$( echo ${my_var} | openssl aes-256-cbc -a -salt -pass pass:somepassword )
+
+original_string=$( echo ${encrypted_pwd} | openssl aes-256-cbc -d -a -pass pass:somepassword)
