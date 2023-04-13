5f9af43188495e466c883a11b99df95a5046025a
Ramkrushna Bolewar <ramkrushna.bolewar1@gmail.com>
2023-04-13 17:59:36 +0530

    Update arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -10,3 +10,20 @@ do
         printf "%2d : %s\n" $index ${arr[$index]}
         #echo -e $index " : " ${arr[$index]}
 done
+
+
+#Declaring array 
+declare -a sport=(
+	[0]=Cricket
+	[1]=Football
+	[2]=Hockey
+	[3]=Tennis
+)
+echo ${sport[2]}
+
+for i in {1..10}
+do
+	num[$i]=$(( $i * $i ))
+done
+
+echo ${num[@]}
